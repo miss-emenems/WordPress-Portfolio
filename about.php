eb19863465f033411ca5fb66cc95c699d2d250de
Martyna <martyna.machowiak@gmail.com>
2018-01-27 19:09:21 +0000

    Set up acf for about me section

--- about.php
+++ about.php
@@ -12,7 +12,51 @@ $section = get_page_by_title('About');
         <div class="column small-12">
             <h1><?php echo get_field('title', $section->ID); ?></h1>
         </div>
-        <div class="column small-12 medium-10 medium-offset-1 end">
-        </div>
     </div>
+
+    <?php
+    $copy = get_field('modules', $section->ID);
+    foreach ($copy as $module) {
+
+        $module = $module['module'];
+
+        switch ($module['settings']['type']) {
+            case "Text only": ?>
+
+                <div class="row expanded small-collapse">
+                    <div class="column small-12 medium-10 medium-offset-1 end">
+                        <div class="about<?php echo $module['settings']['index']?>">
+                            <?php echo $module['copy'] ?>
+                        </div>
+                    </div>
+                </div>
+
+                <?php break;
+            case 'Picture + text': ?>
+
+                <div class="row expanded small-collapse">
+                    <div class="column small-12 medium-4 medium-offset-1">
+                        <div class="about-pic">
+                            <div class="frame"><span></span></div>
+                            <img src="<?php echo $module['picture']['url'] ?>" alt="coming soon...">
+                        </div>
+                    </div>
+                    <div class="column small-12 medium-5 medium-offset-1 end">
+                        <div class="box-padding-vertical">
+                            <div class="about<?php echo $module['settings']['index']?>">
+                                <?php echo $module['copy'] ?>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <?php break;
+        }
+
+    }
+
+    ?>
+
 </section>
